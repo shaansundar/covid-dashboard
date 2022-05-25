5b84dc4acffca2fbe4d048d2cf3ca6bfe3359b7a
shaansundar <shaan.idtindia@gmail.com>
2022-05-25 23:06:48 +0530

    v1.0.0 - Deploy initial Version

---
+++ src/Data/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC_tiI4kP57GKsMG9GQhGjVb919FfdPhNk",
+  authDomain: "covid-tracker-notthatsundar.firebaseapp.com",
+  projectId: "covid-tracker-notthatsundar",
+  storageBucket: "covid-tracker-notthatsundar.appspot.com",
+  messagingSenderId: "122996749730",
+  appId: "1:122996749730:web:0be285e5034d6139636efb",
+  measurementId: "G-NFPSSCH1D6",
+};
